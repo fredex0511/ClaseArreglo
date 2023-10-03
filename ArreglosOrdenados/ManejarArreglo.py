21f3837e2f6d10d083be2661f8051e6a038f95e2
Fredex05 <elcholobrizuela@gmail.com>
2023-10-03 10:43:27 -0600

    Base

---
+++ ArreglosOrdenados/ManejarArreglo.py
@@ -0,0 +1,7 @@
+class ManejarArreglo:
+    def __init__(self,arreglo,caracter,limite,indice_n):
+        arreglo = []
+        caracter = ''
+        limite = 0
+        indice_n = 0
+    def iniciarborrar():
